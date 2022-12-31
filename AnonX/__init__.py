0ebaf39e253f88ee0c836d4dfef57f6a872d6597
EF-YY <121725022+EF-YY@users.noreply.github.com>
2022-12-31 17:24:09 +0200

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,6 +1,6 @@
 from AnonX.core.bot import AnonXBot
 from AnonX.core.dir import dirr
-from AnonX.core.git import git
+
 from AnonX.core.userbot import Userbot
 from AnonX.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
@@ -10,7 +10,6 @@ from .logging import LOGGER
 
 dirr()
 
-git()
 
 dbb()
 
