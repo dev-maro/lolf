52fb009d94cb2f4bc5e607077f86068ad523219d
EF-YY <121725022+EF-YY@users.noreply.github.com>
2023-01-18 01:15:03 +0200

    Update call.py

--- AnonX/core/call.py
+++ AnonX/core/call.py
@@ -6,7 +6,9 @@
 # Please see < https://github.com/TeamAnon/AnonXBot/blob/master/LICENSE >
 #
 # All rights reserved.
-#dev
+#devdnd
+
+
 import asyncio
 from datetime import datetime, timedelta
 from typing import Union
