c0c18fb3eac3a4bab7d6f769a5b2a2a23aa47c8d
EF-YY <121725022+EF-YY@users.noreply.github.com>
2023-01-20 20:52:42 +0200

    Update keybor.py

--- AnonX/plugins/keybor.py
+++ AnonX/plugins/keybor.py
@@ -41,10 +41,6 @@ async def khalid(client: Client, message: Message):
                 )
             )     
 
-      if message.text == 'اخفاء الكيبورد':
-        await message.reply("• تم اخفاء لوحة التحكم لاظهارها مجدداً ارسل /tala",
-        quote=True, reply_markup=ReplyKeyboardRemove (selective=True))
-
 @app.on_message(
     command(["الاوامر"])
     & filters.private
