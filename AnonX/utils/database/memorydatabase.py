0f6271404ea7e399818918907bcea003a5e7f8d5
EF-YY <121725022+EF-YY@users.noreply.github.com>
2023-01-23 07:54:26 +0200

    Update memorydatabase.py

--- AnonX/utils/database/memorydatabase.py
+++ AnonX/utils/database/memorydatabase.py
@@ -217,7 +217,23 @@ async def remove_active_video_chat(chat_id: int):
 
 
 # Delete command mode
+async def is_commanddelete_on(chat_id: int) -> bool:
+    if chat_id not in command:
+        return True
+    else:
+        return False
 
+
+async def commanddelete_off(chat_id: int):
+    if chat_id not in command:
+        command.append(chat_id)
+
+
+async def commanddelete_on(chat_id: int):
+    try:
+        command.remove(chat_id)
+    except:
+        pass
 
 
 # Clean Mode
